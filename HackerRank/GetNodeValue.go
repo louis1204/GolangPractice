5b620d3dfb054c500c80c6ed7eb576210e83b74c
Louis Wong <louis.d.wong@oracle.com>
2020-12-27 19:13:46 -0800

    Rust is the one true language

---
+++ HackerRank/GetNodeValue.go
@@ -0,0 +1,70 @@
+//https://www.hackerrank.com/challenges/get-the-value-of-the-node-at-a-specific-position-from-the-tail/problem
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+type SinglyLinkedListNode struct {
+    data int32
+    next *SinglyLinkedListNode
+}
+
+type SinglyLinkedList struct {
+    head *SinglyLinkedListNode
+    tail *SinglyLinkedListNode
+}
+
+func (singlyLinkedList *SinglyLinkedList) insertNodeIntoSinglyLinkedList(nodeData int32) {
+    node := &SinglyLinkedListNode {
+        next: nil,
+        data: nodeData,
+    }
+
+    if singlyLinkedList.head == nil {
+        singlyLinkedList.head = node
+    } else {
+        singlyLinkedList.tail.next = node
+    }
+
+    singlyLinkedList.tail = node
+}
+
+func printSinglyLinkedList(node *SinglyLinkedListNode, sep string, writer *bufio.Writer) {
+    for node != nil {
+        fmt.Fprintf(writer, "%d", node.data)
+
+        node = node.next
+
+        if node != nil {
+            fmt.Fprintf(writer, sep)
+        }
+    }
+}
+
+// Complete the getNode function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     data int32
+ *     next *SinglyLinkedListNode
+ * }
+ *
+ */
+func getNode(head *SinglyLinkedListNode, positionFromTail int32) int32 {
+    list := []*SinglyLinkedListNode {}
+    for head != nil {
+        list = append(list, head)
+        head = head.next
+    }
+    return list[int32(len(list)) - positionFromTail - 1].data
+}
+
+func main() {
